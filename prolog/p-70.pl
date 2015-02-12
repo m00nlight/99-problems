aadbae3a7de14ced24407d004637984ea55d591c
m00nlight <dot_wangyushi@yeah.net>
2015-02-12 06:08:19 +0000

    Finish prolog problem 70C

--- prolog/p-70.pl
+++ prolog/p-70.pl
@@ -5,3 +5,10 @@ isforest([]).
 isforest([H|T]) :-
     istree(H),
     isforest(T).
+    
+    
+nnodes(t(_, []), 1) :- !.
+nnodes(t(_, Xs), Res) :-
+    maplist(nnodes, Xs, Temp),
+    foldl(plus, Temp, 0, Res0),
+    Res is Res0 + 1.
